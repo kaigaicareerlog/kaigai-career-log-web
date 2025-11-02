d91a6caedcb83ad1898835f80b90e5a118bcd7d8
ryonryon <ryo.togashi.ca@gmail.com>
2025-11-02 00:41:38 -0700

    .

--- src/utils/getTranscriptJsonFilePath.ts
+++ src/utils/getTranscriptJsonFilePath.ts
@@ -5,7 +5,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 export function getTranscriptJsonFilePath(guid: string): string {
-  const transcriptsDir = path.join(__dirname, '../..', 'public', 'transcripts');
+  const transcriptsDir = path.join(process.cwd(), 'public', 'transcripts');
   const jsonPath = path.join(transcriptsDir, `${guid}.json`);
 
   return jsonPath;
